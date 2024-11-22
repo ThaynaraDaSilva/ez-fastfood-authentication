81d41163ebaceebbac1b8f15f457c6beb6ab10d7
Thaynara Silva <thaynara.silva@yaman.com.br>
2024-11-21 22:25:06 -0300

    correção do erro no pom.xml. O ofensor era o espaçamento no param de jwt no application.properties

--- src/test/java/br/com/fiap/ez_fastfood_authentication/EzFastfoodAuthenticationApplicationTests.java
+++ src/test/java/br/com/fiap/ez_fastfood_authentication/EzFastfoodAuthenticationApplicationTests.java
@@ -3,7 +3,9 @@ package br.com.fiap.ez_fastfood_authentication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import br.com.fiap.ez.fastfood.AuthenticationApplication;
+
+@SpringBootTest(classes = AuthenticationApplication.class)
 class EzFastfoodAuthenticationApplicationTests {
 
 	@Test
